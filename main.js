fa4ea6fc3dda2ad16a7bb1febf6109940bbc5822
HARSHHVAISHNAV <harshvaishnav6060@gmail.com>
2024-07-12 17:54:49 +0530

    css1

---
+++ main.js
@@ -0,0 +1,20 @@
+document.getElementById('itema').addEventListener('click', function() {
+    document.getElementById('home').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+document.getElementById('itemb').addEventListener('click', function() {
+    document.getElementById('about').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+document.getElementById('itemc').addEventListener('click', function() {
+    document.getElementById('services').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+document.getElementById('itemd').addEventListener('click', function() {
+    document.getElementById('contact').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
